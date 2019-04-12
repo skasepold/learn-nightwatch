d8c9873f3f04a417406ae2036966acf759f73381
Hindrek Hannus <hindrek.hannus@tptlive.ee>
2019-04-12 17:36:58 +0300

    :i

--- test/e2e/guineaPig.js
+++ test/e2e/guineaPig.js
@@ -1,16 +1,31 @@
 const config = require('../../nightwatch.conf.js');
 
-module.exports = { // addapted from: https://git.io/vodU0
+module.exports = { 
   '@tags': ['guineaPig'],
-  'Guinea Pig Assert Title': function (browser) {
+  'Ducktest': function (browser) {
     browser
-      .url('https://saucelabs.com/test/guinea-pig')
+      .url('http://duckduckgo.com')
       .waitForElementVisible('body')
-      .assert.title('I am a page title - Sauce Labs')
-      .saveScreenshot(`${config.imgpath(browser)}a-screenshot-description.png`)
-      .clearValue('#i_am_a_textbox')
-      .setValue('#i_am_a_textbox', 'nightwatch roolz!')
-      .saveScreenshot(`${config.imgpath(browser)}nightwatch-roolz.png`)
+      .pause(2000)
+      .setValue('input[id=search_form_input_homepage]', 'cat')
+      .saveScreenshot(`${config.imgpath(browser)}duckduck1.png`)
+      .pause(1000)
+      .click('#search_button_homepage')
+      .pause(1000)
+      .assert.title('cat at DuckDuckGo')
+      .assert.containsText('body','cat')
+      .saveScreenshot(`${config.imgpath(browser)}duckduck2.png`)
+      .click('.zcm__link.js-zci-link.js-zci-link--images')
+      .pause(1000)
+      .click('.tile--img__img.js-lazyload')
+      .saveScreenshot(`${config.imgpath(browser)}duckduck3.png`)
+      .pause(2000)
       .end();
   },
 };
+
+    //.assert.title('Google')
+      //.saveScreenshot(`${config.imgpath(browser)}google1.png`)
+      //.assert.title('cat')
+            //.click('input[name=btnK]'
+                  //.saveScreenshot(`${config.imgpath(browser)}google.png`)
